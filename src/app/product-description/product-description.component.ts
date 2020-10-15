785ec0b272afdf1e82ecc4259f1dc1532be2d6e8
dro-1 <lorddro1532@gmail.com>
2020-10-15 22:21:55 +0100

    Stage 2 Done

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from "../product.service";
 
 @Component({
-  selector: 'app-product-description',
-  templateUrl: './product-description.component.html',
-  styleUrls: ['./product-description.component.css']
+  selector: "app-product-description",
+  templateUrl: "./product-description.component.html",
+  styleUrls: ["./product-description.component.css"],
 })
 export class ProductDescriptionComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  albumInfo;
+  constructor(private _productService: ProductService) {}
+  async ngOnInit() {
+    this.albumInfo = (await this._productService.getAlbum(1)).subscribe(
+      (response) => (this.albumInfo = response)
+    );
   }
-
 }
